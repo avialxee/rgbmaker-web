c011a37c2a001ebebc18a113650cc1611cdcee03
Avinash Kumar <avialxee@users.noreply.github.com>
2021-06-07 23:01:56 +0530

    added celery and sqlite

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 # -*- coding: utf-8 -*-
 
-#from wtforms.fields.simple import TextAreaField
 from scripts import forms,smv
-#,helpers,tabledef
 from flask import Flask, redirect, url_for, render_template, request, jsonify
-#,session,flash,
-#import json
-#import sys
 import os
 from flask_cors import CORS
 from flask_restful import reqparse, abort, Api, Resource
+from celery import Celery
+import sqlite3
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.sql.expression import update
+from sqlalchemy import create_engine
 
+
+def make_celery(app):
+    celery = Celery(
+        name='tasks',
+        #backend=app.config["CELERY_BACKEND_URL"],
+        backend='db+sqlite:///db.sqlite3',
+        #broker='amqp://guest:@localhost:5672//',
+        broker='amqps://kxcszysu:GlZGidrId0X_vahdTLSi1ICcTCJ3YRRN@baboon.rmq.cloudamqp.com/kxcszysu',
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
 
 app = Flask(__name__)
 api = Api(app)
@@ -20,11 +38,40 @@ CORS(app)
 #from flask_heroku import Heroku
 #heroku = Heroku(app)
 
+
+# ======== Celery =========================================================== #
+
+celery = make_celery(app)
+
+
+@celery.task(bind=True)
+def get_image(self, arg):
+    info, uri, txt, otext = smv.query(name=arg['name'], position=arg['position'],
+                                      radius=arg['radius'], imagesopt=arg['imagesopt'], archives=arg['archives'])
+    self.update_state(state='PROGRESS' or info,
+                      meta={'txt': txt, 'otext': otext,
+                            'uri': uri})
+    return {'info': info, 'message': txt, 'url': uri, 'brieftext': otext}
+
 # ======== Routing =========================================================== #
 # -------- Login ------------------------------------------------------------- #
 @app.route('/', methods=['GET', 'POST'])
 def login():
     return redirect(url_for('query'))
+
+# -------- Task Status output ------------------------------------------------------------- #
+@app.route('/status/<idv>')
+def taskstatus(idv):
+    task = get_image.AsyncResult(task_id=idv)
+    if task.state == 'PENDING':
+        info = {'info': 'processing'}
+    else:
+        info = task.info
+    try:
+        deleteRecord(task.id)
+    except:
+        print("record not found for task")
+    return info
 
 
 # -------- Query ---------------------------------------------------------- #
@@ -37,7 +84,6 @@ def query():
         radius=request.form['radius']
         archives=request.form['archives']
         images=request.form['images']
-
         
         if qform.validate():
             info, uri, txt, otext = smv.query(name,position,radius,archives,images)
@@ -53,15 +99,11 @@ def query():
 
     return render_template('query.html', form=qform)
 
-
-
 ##-----------==============         API for accessing RGBMaker       ========-----------##
-
 @app.route('/home/<string:num>', methods = ['GET'])
 def disp(num):
     info, uri, txt = smv.query(position=num)
     return jsonify({'info': info, 'message': txt, 'url':uri})
-
 parse = reqparse.RequestParser()
 parse.add_argument('position')
 parse.add_argument('radius')
@@ -69,18 +111,41 @@ parse.add_argument('imagesopt')
 parse.add_argument('archives')
 parse.add_argument('name')
 
-
-
 class RGBMaker(Resource):
     def post(self):
-        arg= parse.parse_args()
-        info, uri, txt, otext = smv.query(name=arg['name'],position=arg['position'], radius=arg['radius'], imagesopt=arg['imagesopt'], archives=arg['archives'])
-        return {'info': info, 'message': txt, 'url':uri, 'brieftext':otext}, 200
+        arg = parse.parse_args()
+        task = get_image.delay(arg)
+        return 202, {'Location': url_for('taskstatus', idv=task.id)}
     def get(self):
         return redirect(url_for('query'))
 api.add_resource(RGBMaker, '/api')
 
 
+# ------------========------------ SQL function ---------=========---------#
+def deleteRecord(tid):
+    """
+    deleting record of each task created by celery.
+    """
+    try:
+        engine = create_engine('sqlite:///db.sqlite3')
+        conn = engine.connect()
+
+        meta = MetaData()
+
+        celery_taskmeta = Table(
+            'celery_taskmeta', meta,
+            Column('task_id', Integer, primary_key=True),
+            Column('status', String),
+        )
+        sql_exec = celery_taskmeta.delete().where(
+            celery_taskmeta.c.task_id == tid, celery_taskmeta.c.status == 'SUCCESS')
+        sql = """DELETE from celery_taskmeta where task_id=?"""
+        conn.execute(sql, (tid,))
+        conn.commit()
+        conn.close()
+        print("status : deleted")
+    except sqlite3.Error as error:
+        print("Failed to delete record from sqlite table", error)
 
 # ======== Main ============================================================== #
 if __name__ == "__main__":
